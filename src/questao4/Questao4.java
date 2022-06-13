50a6c2879f7ce5f584f15e09cbb17dd6d287ebb1
liliane97 <lilianebasilio1997@gmail.com>
2022-06-13 20:28:49 -0300

    Atualizando questão 4 do teste técnico

--- src/questao4/Questao4.java
+++ src/questao4/Questao4.java
@@ -6,21 +6,23 @@ import java.util.Scanner;
 public class Questao4 {
 
 	public static void main(String[] args) {
+		
 		Scanner entrada = new Scanner(System.in);
-		Random geradorDeNumeros = new Random();
-		geradorDeNumeros.nextInt(10);
-		System.out.println("Infome um número: ");
-		int n=0;
-		n = entrada.nextInt();
+		Random gerador = new Random();
+		int gerarNumero = gerador.nextInt(10);
+		int n = 0;
 		
-		
-		if(n == geradorDeNumeros.nextInt()) {
-			System.out.println("Voce acertou");
-		}else {
-			System.out.println("voce errou");
-		}
+		do {
+			System.out.println("Informe um número de 0 a 10: ");
+			n = entrada.nextInt();
+			
+			if(n == gerarNumero) {
+				System.out.println("Parabéns você acertou");
+			}else {
+				System.out.println("Você errou, tente novamente!!!\n");
+			}
+		}while(n !=gerarNumero);
 		
 	
-		
 	}
 }
