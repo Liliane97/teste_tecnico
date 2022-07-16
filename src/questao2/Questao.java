e4d0032830286363736290f2e5f4eabcaf532b47
liliane97 <lilianebasilio1997@gmail.com>
2022-07-16 16:57:08 -0300

    Atualizando questão dois

--- src/questao2/Questao.java
+++ src/questao2/Questao.java
@@ -15,129 +15,113 @@ Scanner entrada = new Scanner(System.in );
 	System.out.println("Informe dez numeros: ");
 	
 	for(int i = 0 ; i < vetor.length;i++) {
-		
 		vetor[i] = entrada.nextInt();
 		soma += vetor[i];
-		
 		for(int y = 0; y < i;y++) {
-			
 			if(vetor[y] == vetor[i]) {
-				
 				System.out.println("Esse numero ja existe, informe outro: ");
 				vetor[i] -= vetor[y];
 				vetor[i]=entrada.nextInt();
-				
 			}	
 		}		
 	}
 //	System.out.println(Arrays.toString(vetor));
 	media = soma /10;
-	System.out.println("Maior numero: " + maiorNumero(vetor));
+	System.out.println("\nMaior numero: " + maiorNumero(vetor));
 	System.out.println("Menor numero: " + menorNumero(vetor));
 	System.out.println("Media aritmetica: " + media);
 
-	System.out.println("Quantidade de numero acima de 10: "+totalDeNumerosMaioresQue10(vetor));
-
-//	System.out.println("Numero maiores que dez: "+ Arrays.toString(mostrarNumerosMaioresQue10(vetor)));
-	System.out.println("Quantidade de numeros acima de 50:  "+totalDeNumerosMaioresQue50(vetor));
-//	System.out.println("Numeros maiores que cinquenta: "+Arrays.toString(mostrarNumerosMaioresQue50(vetor)));
+	System.out.println("Quantidade de numeros maiores que 10 sao: "+totalNumerosMaioresQue10(vetor));
+	
+	mostrarNumerosMaioresQue10(vetor);
+	
+	System.out.println("\nQuantidade de numeros maiores que 50 sao: "+totalDeNumerosMaioresQue50(vetor));
+	
+	mostrarNumerosMaioresQue50(vetor);
 }
 
 public static int maiorNumero(int v[]) {
 	
-	int maior = Integer.MIN_VALUE;
-	
-	for(int i = 0;i<v.length;i++) {
-	
-		if(v[i] > maior) {
-			maior = v[i];
+		int maior = Integer.MIN_VALUE;
+		for(int i = 0;i<v.length;i++) {
+			if(v[i] > maior) {
+				maior = v[i];
+			}
 		}
-	}
-	return maior;
+		
+		return maior;
 }
 
 public static int menorNumero(int v[]) {
 	
-	int menor = Integer.MAX_VALUE;
-	
-	for(int i = 0; i < v.length; i++) {
-		
-		if(v[i] < menor) {
-			menor = v[i];
+		int menor = Integer.MAX_VALUE;
+		for(int i = 0; i < v.length; i++) {
+			if(v[i] < menor) {
+				menor = v[i];
+			}
 		}
-	}
-	return menor;
+		
+		return menor;
 }
 
-public static int totalDeNumerosMaioresQue10(int v[]) {
-	
-	int numerosMaioresQue10 = 0;
-	 
- 	for(int i = 0; i < v.length; i++) {
+public static int totalNumerosMaioresQue10(int v[]) {
 		
- 		if(v[i] > 10 && v[i] < 50) {
-			numerosMaioresQue10++;	
+		int numerosMaioresQue10 = 0;
+	 	for(int i = 0; i < v.length; i++) {
+	 		if(v[i] > 10 && v[i] < 50) {
+				numerosMaioresQue10++;	
+			}
 		}
-	}
-	return numerosMaioresQue10 ;
+	 	
+		return numerosMaioresQue10 ;
 }
 
-
-//public static int [] mostrarNumerosMaioresQue10(int[] v) {
-//	
-//	int maiorQue10[] = new int[totalDeNumerosMaioresQue10(v)] ;
-//	
-//	for(int i =0; i < v.length; i++) {
-//		
-//		for(int y=0; y <= maiorQue10.length-1;y++) {
-//		
-//			if(v[y] >10 && v[y] <50) {
-//				maiorQue10[y]=v[y];
-//			} else {	
-//					maiorQue10[y]= v[y];
-//					v[y]= v[y+1];
-//					v[y+1]= maiorQue10[y]; 
-//			}
-//		}		
-//	}
-//	return  maiorQue10;
-//}
-
-
+public static void mostrarNumerosMaioresQue10(int[] v) {
+	
+		int maiorQue10[]=new int [v.length];
+		for(int i = 0; i < v.length; i++) {
+			if(v[i] > 10 && v[i] < 50 ) {	
+				maiorQue10[i] = v[i];	
+			}		
+		}
+		
+		System.out.println("Os numeros maiores que 10 sao:");
+		for(int maiores: maiorQue10) {
+			if(maiores !=0) {
+		      System.out.println(maiores);
+			}
+		}
+}
 
 public static int totalDeNumerosMaioresQue50(int v[]) {
 	
-	int numerosMaioresQue50 = 0;
-	
-	for(int i = 0; i < v.length; i++) {
+		int numerosMaioresQue50 = 0;
+		for(int i = 0; i < v.length; i++) {
+			if(v[i] > 50) {
+				numerosMaioresQue50++;	
+			}	
+		}
 		
-		if(v[i] > 50) {
-			numerosMaioresQue50++;	
-		}	
-	}
-	return numerosMaioresQue50;
+		return numerosMaioresQue50;
 }
 
-//public static int [] mostrarNumerosMaioresQue50(int[] v) {
-//	
-//	int maiorQue50[] = new int[totalDeNumerosMaioresQue50(v)] ;
-//	
-//	for(int i =0; i < v.length; i++) {
-//		
-//		for(int y=0; y < maiorQue50.length;y++) {
-//		
-//			if(v[y] > 50) {
-//				maiorQue50[y]=v[y];
-//			} else {
-//					maiorQue50[y]= v[y];
-//					v[y]= v[y+1];
-//					v[y+1]= maiorQue50[y]; 
-//			}
-//			
-//		}	
-//		
-//	}
-//	return  maiorQue50;
-//}
+public static  void mostrarNumerosMaioresQue50(int[] v) {
+
+		int maiorQue50[] = new int[v.length];
+		for(int i =0; i < v.length; i++) {	
+			for(int y=0; y < maiorQue50.length;y++) {
+				if(v[y] > 50) {
+					maiorQue50[y]=v[y];
+				} 
+			}	
+		}
+		
+		System.out.println("Os numeros maiores que 50 sao: ");
+		for(int maiores : maiorQue50) {
+			if(maiores !=0) {
+				System.out.println(maiores);
+			}
+		}	
+	}
 
 }
